ca839155d3eff53faefbbdf866e5e2c2fe4fff8e
Bodaniel Jeanes <me@bjeanes.com>
2009-05-05 15:59:59 +1000

    added enclosures to normal RSS

--- lib/feedzirra/parser/rss_entry.rb
+++ lib/feedzirra/parser/rss_entry.rb
@@ -33,6 +33,10 @@ module Feedzirra
       elements :category, :as => :categories
 
       element :guid, :as => :id
+      
+      element :enclosure, :value => :length, :as => :enclosure_length
+      element :enclosure, :value => :type, :as => :enclosure_type
+      element :enclosure, :value => :url, :as => :enclosure_url
     end
 
   end
